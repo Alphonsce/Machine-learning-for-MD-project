c4d8c0daea37203f2b4fcbb2234835f066773b60
Alexander Varlamov <varlamow.sas@gmail.com>
2023-01-31 00:12:38 +0200

    d_velocity dataset is created

--- integration.py
+++ integration.py
@@ -4,10 +4,23 @@ def predict_vel(X, v0, model, model_type="sklearn"):
 def predict_movement(X, v, model, model_type="sklearn"):
     pass
 
+def fill_xyz():
+    '''
+    fills xyz file after current step
+    '''
+    pass
+
+def fill_csv():
+    '''
+    fills csv after current step
+    '''
+    pass
+
 def integration_step():
     '''
     returns: movement
     '''
+    pass
 
 def integration_cycle(starting_coords, starting_vels):
     pass
